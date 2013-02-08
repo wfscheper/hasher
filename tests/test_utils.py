681abdfe1754f3f41eb4ee82d8958e9304ee1362
Walter Scheper <ratlaw@gmail.com>
2013-02-08 08:23:07 -0500

    Expand test coverage of test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import hashlib
 
-from ..utils import check_re
+from hasher.hashes.base import check_re
 
 
 class TestCheckRegex(object):
@@ -8,18 +8,48 @@ class TestCheckRegex(object):
 
     def setup_method(self, method):
         self.filename = 'myfile.txt'
-        self.md5_hash = hashlib.md5('This is a test string').hexdigest()
+        self.hash = hashlib.md5('This is a test string').hexdigest()
 
     def test_valid_string(self):
-        string = '%s  %s' % (self.filename, self.md5_hash)
+        string = '%s  %s' % (self.hash, self.filename)
         match = check_re.match(string)
 
         assert match is not None
-        assert match.groups() == (self.filename, None, self.md5_hash)
+        assert match.groups() == (self.hash, None, self.filename)
 
     def test_binary_string(self):
-        string = '%s  *%s' % (self.filename, self.md5_hash)
+        string = '%s  *%s' % (self.hash, self.filename)
         match = check_re.match(string)
 
         assert match is not None
-        assert match.groups() == (self.filename, '*', self.md5_hash)
+        assert match.groups() == (self.hash, '*', self.filename)
+
+    def test_missing_hash(self):
+        string = "{hash} ".format(hash=self.hash)
+        assert check_re.match(string) is None
+
+    def test_missing_filename(self):
+        string = "  {file}".format(file=self.filename)
+        assert check_re.match(string) is None
+
+    def test_missing_flag(self):
+        string = "{hash} {file}".format(hash=self.hash, file=self.filename)
+        assert check_re.match(string) is None
+
+    def test_wrong_flag(self):
+        string = "{hash} +{file}".format(hash=self.hash, file=self.filename)
+        assert check_re.match(string) is None
+
+    def test_filename_with_slash(self):
+        filename = "filewith\\slash.txt"
+        string = "{hash}  {file}".format(hash=self.hash, file=filename)
+        match = check_re.match(string)
+        assert match is not None
+        assert match.groups() == (self.hash, None, filename)
+
+    def test_filename_with_space(self):
+        filename = "filewith space.txt"
+        string = "{hash}  {file}".format(hash=self.hash, file=filename)
+        match = check_re.match(string)
+        assert match is not None
+        assert match.groups() == (self.hash, None, filename)
